a5b2c7901c56bae841280d496af629eb7df8444e
thunfischbrot <moritz@moritzsuess.de>
2016-11-20 19:38:46 +0100

    Updated to refer to ext6 folder

--- proxmox_noreminder.sh
+++ proxmox_noreminder.sh
@@ -13,11 +13,11 @@ if [ "$1" == "pvemanagerlib.js.dpkg-tmp" ]; then
     sleep 10
 
     # patch the files
-    cp /usr/share/pve-manager/ext4/pvemanagerlib.js /usr/share/pve-manager/ext4/pvemanagerlib.js.bak
-    sed -i -r -e "s/if \(data.status !== 'Active'\) \{/if (false) {/" /usr/share/pve-manager/ext4/pvemanagerlib.js >>/var/log/incron.log 2>&1
-    sed -i -r -e "s/You do not have a valid subscription for this server/This server is receiving updates from the Proxmox VE No-Subscription Repository/" /usr/share/pve-manager/ext4/pvemanagerlib.js >>/var/log/incron.log 2>&1
-    sed -i -r -e "s/No valid subscription/Community Edition/" /usr/share/pve-manager/ext4/pvemanagerlib.js >>/var/log/incron.log 2>&1
+    cp /usr/share/pve-manager/ext6/pvemanagerlib.js /usr/share/pve-manager/ext6/pvemanagerlib.js.bak
+    sed -i -r -e "s/if \(data.status !== 'Active'\) \{/if (false) {/" /usr/share/pve-manager/ext6/pvemanagerlib.js >>/var/log/incron.log 2>&1
+    sed -i -r -e "s/You do not have a valid subscription for this server/This server is receiving updates from the Proxmox VE No-Subscription Repository/" /usr/share/pve-manager/ext6/pvemanagerlib.js >>/var/log/incron.log 2>&1
+    sed -i -r -e "s/No valid subscription/Community Edition/" /usr/share/pve-manager/ext6/pvemanagerlib.js >>/var/log/incron.log 2>&1
 
     # log  the changes
-    diff /usr/share/pve-manager/ext4/pvemanagerlib.js.bak /usr/share/pve-manager/ext4/pvemanagerlib.js >> /var/log/incron.log
+    diff /usr/share/pve-manager/ext6/pvemanagerlib.js.bak /usr/share/pve-manager/ext6/pvemanagerlib.js >> /var/log/incron.log
 fi
