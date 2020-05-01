4196ef8269f5bdaaf85c3ff050716bd333c0d014
chriswayg <chriswayg@users.noreply.github.com>
2020-05-02 02:15:43 +0800

    Updated for Proxmox 6

--- proxmox_noreminder.sh
+++ proxmox_noreminder.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 # automatic removal of Proxmox subscription reminder during upgrades
-# - tested with Proxmox 3.4 and 4.0
+# - tested with Proxmox 6
 #
 # exit on error
 set -e
 
+#echo "--- File changed: $1" >> /var/log/incron.log
+
 # Since we are watching the whole directory, we need to check for the correct file
-if [ "$1" == "pvemanagerlib.js.dpkg-tmp" ]; then
-    echo "$(date +%Y-%m-%d_%H:%M) pvemanagerlib.js has been upgraded - patching file" >> /var/log/incron.log
+if [ "$1" == "proxmoxlib.js.dpkg-tmp" ]; then
+    echo "$(date +%Y-%m-%d_%H:%M) proxmoxlib.js has been upgraded - patching file" >> /var/log/incron.log
 
     # wait a bit until the file has its permanent name
-    sleep 10
+    sleep 15
 
     # patch the files
-    cp /usr/share/pve-manager/ext4/pvemanagerlib.js /usr/share/pve-manager/ext4/pvemanagerlib.js.bak
-    sed -i -r -e "s/if \(data.status !== 'Active'\) \{/if (false) {/" /usr/share/pve-manager/ext4/pvemanagerlib.js >>/var/log/incron.log 2>&1
-    sed -i -r -e "s/You do not have a valid subscription for this server/This server is receiving updates from the Proxmox VE No-Subscription Repository/" /usr/share/pve-manager/ext4/pvemanagerlib.js >>/var/log/incron.log 2>&1
-    sed -i -r -e "s/No valid subscription/Community Edition/" /usr/share/pve-manager/ext4/pvemanagerlib.js >>/var/log/incron.log 2>&1
+    cp /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js.bak
+    sed -i "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+    systemctl restart pveproxy.service
 
     # log  the changes
-    diff /usr/share/pve-manager/ext4/pvemanagerlib.js.bak /usr/share/pve-manager/ext4/pvemanagerlib.js >> /var/log/incron.log
+    diff /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js.bak /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js >> /var/log/incron.log
 fi
